8cff3d2511bbcccd9d92fc35703daf72e74147e7
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-07-15 12:44:09 +0100

    Added files and began to add object constructors

---
+++ logic.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+        if (this.read == true) {
+            return `${this.title} by ${this.author}, ${this.pages} pages, read`;
+        } else {
+            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+        }
+    };
+}
+
+let bookOne = new Book('Harry Potter', 'J.K Rowling', 255, true);
+console.log(bookOne.info());
