6eadc4264129d5755ba3be9ad15b99100a355387
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-07-20 14:26:19 +0100

    a function that loops through the array and displays each book on the page and styled it a bit

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const container = document.querySelector(".book-card-container");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -19,3 +20,17 @@ myLibrary[0] = new Book("The Hobbit", "John Ronald Reuel Tolkien", 320, false);
 function addBookToLibrary(title, author, pages, read) {
     myLibrary[myLibrary.length] = new Book(title, author, pages, read);
 };
+
+function displayBookToLibrary() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `<p><strong>Title:</strong> ${myLibrary[i].title}</p>
+        <p><strong>Author:</strong> ${myLibrary[i].author}</p>
+        <p><strong>Pages:</strong> ${myLibrary[i].pages}</p>
+        <p><strong>Read:</strong> ${myLibrary[i].read}</p>`;
+        container.appendChild(card);
+    }
+}
+
+displayBookToLibrary();
