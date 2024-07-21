354d71623d3ae1f5bed3c563a73f07ebd028e294
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-07-21 11:01:33 +0100

    Figured out how to display new book to the card

--- logic.js
+++ logic.js
@@ -3,6 +3,9 @@ const container = document.querySelector(".book-card-container");
 const dialog = document.querySelector("dialog");
 const newBook = document.querySelector("#new-book");
 const closeButton = document.querySelector("#close-button");
+const inputs = document.querySelectorAll("input");
+const submit = document.querySelector("#submit");
+const inputValues = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,15 +13,11 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        if (this.read == true) {
-            return `${this.title} by ${this.author}, ${this.pages} pages, read`;
-        } else {
-            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
-        }
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     };
 }
 
-myLibrary[0] = new Book("The Hobbit", "John Ronald Reuel Tolkien", 320, false);
+myLibrary[0] = new Book("The Hobbit", "John Ronald Reuel Tolkien", 320, "Not read yet");
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary[myLibrary.length] = new Book(title, author, pages, read);
@@ -31,7 +30,7 @@ function displayBookToLibrary() {
         card.innerHTML = `<p><strong>Title:</strong> ${myLibrary[i].title}</p>
         <p><strong>Author:</strong> ${myLibrary[i].author}</p>
         <p><strong>Pages:</strong> ${myLibrary[i].pages}</p>
-        <p><strong>Read:</strong> ${myLibrary[i].read}</p>`;
+        <p><strong>Status:</strong> ${myLibrary[i].read}</p>`;
         container.appendChild(card);
     }
 }
@@ -42,6 +41,11 @@ newBook.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     dialog.close();
+});
+
+submit.addEventListener("click", () => {
+    addBookToLibrary(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value);
+    displayBookToLibrary();
 })
 
 displayBookToLibrary();
